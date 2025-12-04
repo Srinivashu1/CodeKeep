cb8664dc2e82770b917c17e05e82dfedd2426e40
Srinivash <srinivashu20@gmail.com>
2025-12-04 17:20:41 +0530

    Update: Your commit message here

--- records/code.js
+++ records/code.js
@@ -1,6 +1,6 @@
-const records = [
+const Rec2 = [
     {
-        id: 1,
+        id: 100,
         code: "Add Github",
         description: `Initialize and push to GitHub`,
         command: `git init
@@ -12,7 +12,7 @@ git push -u origin main`,
         category: "Git"
     },
     {
-        id: 2,
+        id: 101,
         code: "Update Github",
         description: `Commit and push changes`,
         command: `
@@ -20,28 +20,96 @@ git add .
 git commit -m "Update: Your commit message here"
 git push`,
         category: "Git"
-    },
-    // {
-    //     id: 3,
-    //     code: "Create React App",
-    //     description: `
-    //     Create a new React application`,
-    //     command: `
-    //     npx create-react-app my-app
-    //     cd my-app
-    //     npm start`,
-    //     category: "React"
-    // },
-    // {
-    //     id: 4,
-    //     code: "Install Dependencies",
-    //     description: 
-    //     `Install project dependencies`,
-    //     command: `
-    //     npm install
-    //     # or with yarn
-    //     yarn install`,
-    //     category: "Node.js"
-    // }
+    }
+
 ];
+
+
+const Rec1 = [
+    {
+        category: "CodeIgniter",
+        id: 3,
+        code: "Web 404 Error",
+        description: `
+        Customize the 404 error page in CodeIgniter`,
+        command: `
+$routes->set404Override(function () {
+    return view('errors/custom404');
+});
+`
+    }
+    ,
+    {
+        id: 3,
+        category: "CodeIgniter",
+        code: "Auth Middleware",
+        description: `Implement authentication middleware in CodeIgniter to protect routes from unauthorized access.`,
+        command: `
+public function __construct()
+{
+    $this->session = session();
+    $this->router = service('router');
+    $controller = class_basename($this->router->controllerName());
+    $method = $this->router->methodName();
+    $currentRoute = $controller . '/' . $method;
+    $excludedRoutes = [
+        'controller/login',
+        'controller/sendOtp',
+        'controller/sendSMS',
+    ];
+
+    $logger = json_decode($_COOKIE["logger"] ?? null);
+    $isLoggedIn = $logger->loggedIn ?? null;
+
+    if (empty($isLoggedIn)  && !in_array($currentRoute, $excludedRoutes, true)) {
+        //Inaconstructoryoucan't"return"aresponse;senditandexit
+        echo '<script>window.location.href= "' . base_url() . '";</script>';
+        die();
+    }
+}
+    `
+    }, {
+        id: 4,
+        category: "CodeIgniter",
+        code: "Data Encryption",
+        description: `Encrypt data using OpenSSL in CodeIgniter`,
+        command: `
+public function dataEncrypt($value)
+{
+    $key    = "mysecretkey12345678901234567890"; // 32 chars (AES-256)
+    $iv     = openssl_random_pseudo_bytes(16);
+    $cipher = "AES-256-CBC";
+    $encrypted = openssl_encrypt($value, $cipher, $key, 0, $iv);
+    return base64_encode($iv . $encrypted);
+}
+
+`
+    }
+    , {
+        id: 5,
+        category: "CodeIgniter",
+        code: "Data Decryption",
+        description: `Decrypt data using OpenSSL in CodeIgniter`,
+        command: `
+public function dataDecrypt($encryptedData)
+{
+    $key       = "mysecretkey12345678901234567890"; // same key
+    $cipher    = "AES-256-CBC";
+
+    $data      = base64_decode($encryptedData);
+
+    $iv        = substr($data, 0, 16);
+    $encrypted = substr($data, 16);
+
+    return openssl_decrypt($encrypted, $cipher, $key, 0, $iv);
+}
+`    }, {
+
+    }
+]
+
+
+const records = [...Rec1, ...Rec2].sort((a, b) => a.id - b.id);
+
+
 
